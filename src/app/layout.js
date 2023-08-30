085fad8acae9fc6ab4061c3fb6aa6862b3f05376
itscopebd <itscopebd@gmail.com>
2023-08-30 14:58:35 +0600

    when user registration or login with google then save data user in database

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,26 @@
+"use client"
+
 import { AuthProvider } from '@/context/authContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Blood Bond Network',
-  description: 'This is Blood Bond Network',
-}
+const queryClient = new QueryClient()
+// export const metadata = {
+//   title: 'Blood Bond Network',
+//   description: 'This is Blood Bond Network',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      
         <AuthProvider>
+        <QueryClientProvider client={queryClient}>
             {children}
+            </QueryClientProvider>
         </AuthProvider>
       
         
