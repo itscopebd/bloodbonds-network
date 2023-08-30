7210a3fd8d55dac736d5b391ba908faef389329e
ashraf56 <ashrafulfahim07@gmail.com>
2023-08-30 12:20:22 +0600

    dashboard created & and set button in the log in and signup page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -2,11 +2,17 @@ import React from 'react';
 import SignupItem from './SignupItem';
 import Image from 'next/image';
 import img from '../asset/signup.png'
+import Link from 'next/link';
 
 const SignupPage = () => {
     return (
-        <div>
-                 <div className="hero min-h-screen bg-base-200">
+        <div className='bg-base-200'>
+               <div className='px-5 pt-1 '>
+     <button className='btn w-30'>
+        <Link  href={'/'} >  <span className='ps-1'>Back To Home</span></Link>
+      </button>
+     </div>
+                 <div className="hero min-h-screen ">
         <div className="hero-content flex-col lg:flex-row card dark:text-white ">
           <div className='w-3/6 '>
             <Image src={img} alt='signup' width={700} />
