6101bca1e4076a4627ddd1f30afb33ec0cc77d9b
itscopebd <itscopebd@gmail.com>
2023-09-11 02:07:42 -0700

    Donors applied process added

---
+++ src/app/api/donor/route.js
@@ -0,0 +1,20 @@
+import { connectToDatabase } from "@/utils/database";
+import { NextResponse } from "next/server";
+export const POST = async (request) => {
+  const donorData = await request.json();
+  const { db } = await connectToDatabase();
+  const donorEmail = donorData.email;
+  try {
+    const checkDonorEmail = await db
+      .collection("donors")
+      .findOne({ email: donorEmail });
+    if (checkDonorEmail) {
+      return NextResponse.json({ message: "You Already applied !" });
+    } else {
+      const result = await db.collection("donors").insertOne(donorData);
+      return NextResponse.json(result);
+    }
+  } catch (error) {
+    return NextResponse.json({ message: error.toString() });
+  }
+};
