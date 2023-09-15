f4c82a8496c97cadf783afe79c51fb0b7c382489
itscopebd <itscopebd@gmail.com>
2023-09-14 22:01:24 -0700

    update admin status and delete admin

---
+++ src/app/dashboard/admin/alladmin/MakeUser.jsx
@@ -0,0 +1,45 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FaUser } from "react-icons/fa";
+import Swal from "sweetalert2";
+
+const MakeUser = ({ id }) => {
+    const router= useRouter()
+  const handleMakeUser = async () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "This Admin is User!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, User it!",
+    }).then(async (response) => {
+      if (response.isConfirmed) {
+        const data = "user";
+        const res = await fetch(`/api/admin/${id}`, {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (res.ok) {
+          router.refresh();
+          Swal.fire("Yes!", "This Admin is User.", "success");
+        }
+      }
+    });
+  };
+  return (
+    <button
+      onClick={handleMakeUser}
+      className="btn btn-sm capitalize hover:bg-black hover:text-white text-white bg-secondaryColor"
+      title="Make a user"
+    >
+      <FaUser className="w-full h-full p-2" />
+    </button>
+  );
+};
+export default MakeUser;
