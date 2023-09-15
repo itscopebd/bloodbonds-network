5309e9ef0ee0cfe8d34f12ab1047e3798df22bd2
itscopebd <itscopebd@gmail.com>
2023-09-15 12:52:35 -0700

    approve donor folder move

--- src/app/api/approvedonor/route.js
+++ src/app/api/approvedonor/route.js
@@ -1,7 +1,8 @@
 import { connectToDatabase } from "@/utils/database";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request) => {
+  console.log(" approve ", request)
   const query = { status: "Approve" };
   const { db } = await connectToDatabase();
   const result = await db.collection("donors").find(query).toArray();
