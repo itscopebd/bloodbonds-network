094df8dec7e7af4e2d571ad979c4208a78d7f348
itscopebd <itscopebd@gmail.com>
2023-09-15 17:08:53 -0700

    added revalidate

--- src/app/dashboard/admin/applieddonor/AppliedDonor.jsx
+++ src/app/dashboard/admin/applieddonor/AppliedDonor.jsx
@@ -8,7 +8,7 @@ const baseUrl= process.env.Base_URL
 const getAllAppliedDonor= async()=>{
 
   try {
-    const res= await fetch(`${baseUrl}/api/donor/pending`,{cache:"no-store"})
+    const res= await fetch(`${baseUrl}/api/donor/pending`,{cache:"no-store",next:{revalidate:0}})
     if (!res.ok) {
       throw Error("Data no loading")
     }
