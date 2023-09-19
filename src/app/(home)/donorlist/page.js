5a8179ffe56402bef79212d1ccb8b371f44b621f
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-19 11:15:30 +0600

    donor page UI modified

--- src/app/(home)/donorlist/page.js
+++ src/app/(home)/donorlist/page.js
@@ -1,12 +1,30 @@
-import DonorBanner from '@/components/Donorlist/DonorBanner';
 import Donorlist from '@/components/Donorlist/Donorlist';
-import PlasmaBanner from '@/components/PlasmaBanner/PlasmaBanner';
 import React from 'react';
 
 const AlldOnorList = () => {
     return (
         <div>
-          <DonorBanner></DonorBanner>
+                     <div className="hero h-96" style={{backgroundImage: 'url(https://i.ibb.co/JnXWzXd/27577817-ravi24-may-7-1.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-white">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Blood Heroes: Find Your Lifesaver</h1>
+      <p className="mb-5">Search Donors by Blood Type and Save Lives Today</p>
+      <select className="select select-secondary w-full max-w-xs text-black  dark:text-white">
+  <option  selected>Search your  Blood</option>
+  <option value="A+">A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+">B+</option>
+                    <option value="B-">B-</option>
+                    <option value="AB+">AB+</option>
+                    <option value="AB-">AB-</option>
+                    <option value="O+">O+</option>
+                    <option value="O-">O-</option>
+</select>
+    </div>
+  </div>
+</div>
+
           <Donorlist></Donorlist>
         </div>
     );
--- src/components/Donorlist/Donorlist.jsx
+++ src/components/Donorlist/Donorlist.jsx
@@ -22,29 +22,29 @@ const Donorlist = async () => {
 const alldonor= await getApproveDonor();
 
     return (
-        <div className='my-10'>
+        <div className='my-10 container mx-auto'>
             <div>
                 <h1 className='px-12 font-medium uppercase py-5 text-xl'>All Donor</h1>
             </div>
-            <div className='grid grid-cols-1    lg:grid-cols-2   gap-3 mx-auto justify-center p-10'>
+            <div className='grid grid-cols-1    lg:grid-cols-3  gap-3 mx-auto justify-center p-5'>
 {
     alldonor.map(ad =>(
-        <div className="card xl:card-side  max-w-full h-full bg-base-100 shadow-xl" key={ad._id}>
+        <div className="card xl:card-side max-w-full h-full bg-base-100 shadow-2xl" key={ad._id}>
  
   <div className="avatar">
-  <div className="  w-52 rounded-br-full   ">
-     <Image src={ad.image} width={500} height={500} alt='donor image' />
+  <div className="w-48  lg:w-36 rounded-br-full  ">
+     <Image src={ad.image} width={300} height={300} alt='donor image' />
   </div>
 </div>
 
-  <div className="card-body mt-0 md:-mt-20 lg:mt-0 text-start md:text-center lg:text-center xl:text-start ">
+  <div className="card-body  mt-0 md:-mt-20 lg:mt-0 text-start md:text-center lg:text-center xl:text-start ">
     <h2 className="font-bold text-2xl">{ad.name}</h2>
-    <div className='card'>
+   
+    <div className='card text-sm h-full'>
         <p>{ad.paraddress}</p>
         <p> {ad.blood}</p>
         <p> {ad.gender}</p>
-        <p className='text-xs md:text-base'> {ad.email}</p>
-      
+       <p className='text-xs md:text-sm'> {ad.email}</p>
     </div>
     <div className="text-start md:text-center  lg:text-center xl:text-start ">
        <button className="btn btn-secondary  btn-sm  ">Contact Now</button>
