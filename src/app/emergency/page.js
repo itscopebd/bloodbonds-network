45c2760f9b793b0af2fa855c65f6c8cd3bec7c67
farjanarahman <rahmanfarjana313@gmail.com>
2023-09-22 05:01:00 +0600

    emergency page responsive done

--- src/app/emergency/page.js
+++ src/app/emergency/page.js
@@ -73,22 +73,27 @@ const Emergency = () => {
               required
             />
           </div>
-          <div className='flex mt-3'>
-            <label className='text-md md:text-lg font-bold label cursor-pointer'>Recipients:</label>
-            {bloodTypes.map((bloodType) => (
-              <label className='label cursor-pointer'>
-                <span key={bloodType}></span>
-                <input
-                  type="checkbox"
-                  className='ms-2 me-2 checkbox'
-                  value={bloodType}
-                  checked={recipients.includes(bloodType)}
-                  onChange={handleRecipientChange}
-                />
-                {bloodType}
-              </label>
-            ))}
-          </div>
+          <div className='flex mt-3 flex-wrap'>
+  <label className='text-md md:text-lg font-bold label cursor-pointer'>
+    Recipients:
+  </label>
+  <div className='flex flex-wrap'>
+    {bloodTypes.map((bloodType) => (
+      <label className='label cursor-pointer' key={bloodType}>
+        <span key={bloodType}></span>
+        <input
+          type="checkbox"
+          className='ms-2 me-2 checkbox '
+          value={bloodType}
+          checked={recipients.includes(bloodType)}
+          onChange={handleRecipientChange}
+        />
+        {bloodType}
+      </label>
+    ))}
+  </div>
+</div>
+
           <div className='w-full max-w-xs mt-3'>
             <label htmlFor="urgencyLevel" className='text-md md:text-lg font-bold'>Urgency Level:</label>
             <select
@@ -109,6 +114,75 @@ const Emergency = () => {
           </div>
           <button type="submit" className='mx-auto w-48 btn btn-error mt-5'>Send Notification</button>
         </form>
+//         <form
+//   onSubmit={handleSubmit}
+//   className='mt-10 justify-center mx-auto bg-red-50 p-10 card flex-shrink-0 w-full shadow-2xl max-w-4xl rounded-lg py-8 px-10 flex flex-grow overflow-y-auto'
+// >
+//   <div className='flex flex-col md:flex-row'>
+//     <div className='md:w-1/2 md:pr-4'>
+//       <label htmlFor='message' className='text-md md:text-lg font-bold'>
+//         Message:
+//       </label>
+//       <textarea
+//         id='message'
+//         placeholder='Message'
+//         className='ms-2 p-2 border border-red-700'
+//         value={message}
+//         rows={4}
+//         cols={60}
+//         onChange={handleMessageChange}
+//         required
+//       />
+//     </div>
+//     <div className='md:w-1/2 md:pl-4'>
+//       <label className='text-md md:text-lg font-bold label cursor-pointer'>
+//         Recipients:
+//       </label>
+//       {bloodTypes.map((bloodType) => (
+//         <label className='label cursor-pointer' key={bloodType}>
+//           <input
+//             type='checkbox'
+//             className='ms-2 me-2 checkbox'
+//             value={bloodType}
+//             checked={recipients.includes(bloodType)}
+//             onChange={handleRecipientChange}
+//           />
+//           {bloodType}
+//         </label>
+//       ))}
+//     </div>
+//   </div>
+//   <div className='w-full max-w-xs mt-3'>
+//     <label htmlFor='urgencyLevel' className='text-md md:text-lg font-bold'>
+//       Urgency Level:
+//     </label>
+//     <select
+//       id='urgencyLevel'
+//       value={urgencyLevel}
+//       className='ms-2 p-2 rounded'
+//       onChange={(e) => setUrgencyLevel(e.target.value)}
+//     >
+//       <option value='high'>High</option>
+//       <option value='medium'>Medium</option>
+//       <option value='low'>Low</option>
+//     </select>
+//   </div>
+//   <div className='flex mt-3'>
+//     <label className='text-md md:text-lg font-bold'>Message Preview:</label>
+//     <textarea
+//       className='message-preview border border-red-700 italic overflow-y-auto ms-2 p-2'
+//       placeholder='Preview Message'
+//       rows={4}
+//       cols={50}
+//     >
+//       {message}
+//     </textarea>
+//   </div>
+//   <button type='submit' className='mx-auto w-48 btn btn-error mt-5'>
+//     Send Notification
+//   </button>
+// </form>
+
       )}
       <div>
         <h2 className='text-center text-2xl md:text-3xl text-red-700 font-bold mb-5 mt-10'>Emergency Tips</h2>
