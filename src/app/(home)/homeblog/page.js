f86361fc1169487c894822b8a8e53093460bb0a2
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-23 15:29:17 +0600

    blogs added

---
+++ src/app/(home)/homeblog/page.js
@@ -0,0 +1,12 @@
+import Blog from '@/components/Blog/Blog';
+import React from 'react';
+
+const page = async () => {
+    return (
+        <div>
+            <Blog/>
+        </div>
+    );
+};
+
+export default page;
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,34 +1,30 @@
-
-import React from 'react';
-import { FaRegCalendar } from 'react-icons/fa6';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaUserAlt } from 'react-icons/fa';
+import React from 'react';
+import { FaRegCalendar, FaUserAlt } from 'react-icons/fa';
 
-const getActiveBlog = async  () =>{
-  let base_url = process.env.Base_URL;
+const getActiveBlog = async () =>{
+const baseURl= process.env.Base_URL
 try {
-  let res= await fetch(`${base_url}/api/blog/active`,{
-    cache:"no-store"
+  const res= await fetch (`${baseURl}/api/blog/active`,{
+    cache: "no-store"
   })
-  if (!res.ok) {
-    throw  new Error("data load failed")
-  }
+if (!res.ok) {
+  throw new Error("data fetch error")
+}
   return res.json()
 } catch (error) {
   console.log(error);
 }
 
-
 }
 
-const Blog = async() => {
+const Blog = async () => {
 
-  let activeblog = await getActiveBlog()
-
+  const allblogs= await getActiveBlog()
   return (
-    // The bloge page section 
-    <div className='text-center py-20 container mx-auto'>
+    <div>
+        <div className='text-center py-20 container mx-auto'>
       <h1 className="text-5xl text-center font-bold text-red-500 ">Our Blogs</h1>
       <hr className='w-44 mt-2 mb-4 rounded mx-auto border border-red-500' />
       <p className='py-2 font-bold'>Let's all serve everyone by donating blood..</p>
@@ -36,7 +32,7 @@ const Blog = async() => {
       <div className='grid  grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3'>
 
      {
-activeblog.map((blg) => (
+allblogs.map((blg) => (
   <div className="card w-96 h-full mt-4 mx-auto bg-base-100 shadow-xl rounded-none" key={blg._id}>
   <figure>
     <Image src={blg.image} width={400} height={400} alt="Food" className='p-2 w-screen h-[242px]' />
@@ -46,7 +42,7 @@ activeblog.map((blg) => (
       {blg.title}
     </h1>
     <p>{blg.content.slice(0,150)}
-    <Link href={`/blogdetail/${blg._id}`}> <span className=" text-red-500 ">Read full</span>  </Link>
+    <Link href={`/homeblog/blogdetail/${blg._id}`}> <span className=" text-red-500 ">Read full</span>  </Link>
     </p>
     <hr className='border-red-400 border w-14 mx-auto my-2' />
     <div className="card-actions mx-auto text-slate-400">
@@ -63,6 +59,7 @@ activeblog.map((blg) => (
 
 
       </div>
+    </div>
     </div>
   );
 };
