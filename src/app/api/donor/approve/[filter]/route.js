ee0270c1b4582b2935ed13312421d1bc462a860c
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-28 11:37:36 +0600

    search sytem modified

--- src/app/api/donor/approve/[filter]/route.js
+++ src/app/api/donor/approve/[filter]/route.js
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 export const revalidate = 0;
 export const GET = async (request, { params }) => {
   const { filter } = params;
+
   const { db } = await connectToDatabase();
 
   try {
     const query = { blood: filter };
+   
     const result = await db.collection("donors").find(query).toArray();
     return NextResponse.json(result);
   } catch (err) {
--- src/components/Donorlist/Donorlist.jsx
+++ src/components/Donorlist/Donorlist.jsx
@@ -3,25 +3,31 @@ import Image from "next/image";
 import React from "react";
 
 const Donorlist = ({ donor }) => {
- 
+  const isLoading = !donor;
+  const isNoData = !isLoading && donor.length === 0;
+
   return (
     <div className="my-10 container mx-auto">
       <div>
         <h1 className="px-12 font-medium uppercase py-5 text-xl">All Donor</h1>
       </div>
-    { donor.length === 0 ? (
+    { isLoading ? (
+        <div className="text-center">Loading...</div>
+      ) :  
+    isNoData ? (
       <div>no data</div>
     ) : (
       <div className="grid grid-cols-1    lg:grid-cols-3  gap-3 mx-auto justify-center p-5">
-        {donor.map((ad) => (
+        { 
+        donor?.map((ad) => (
           <div
             className="card xl:card-side max-w-full h-full bg-base-100 shadow-2xl"
-            key={ad._id}
+            key={ad?._id}
           >
             <div className="avatar">
               <div className="w-48  lg:w-36 rounded-br-full  ">
                 <Image
-                  src={ad.image}
+                  src={ad?.image}
                   width={300}
                   height={300}
                   alt="donor image"
@@ -30,13 +36,13 @@ const Donorlist = ({ donor }) => {
             </div>
 
             <div className="card-body  mt-0 md:-mt-20 lg:mt-0 text-start md:text-center lg:text-center xl:text-start ">
-              <h2 className="font-bold text-2xl">{ad.name}</h2>
+              <h2 className="font-bold text-2xl">{ad?.name}</h2>
 
               <div className="card text-sm h-full">
-                <p>{ad.paraddress}</p>
-                <p> {ad.blood}</p>
-                <p> {ad.gender}</p>
-                <p className="text-xs md:text-sm"> {ad.email}</p>
+                <p>{ad?.paraddress}</p>
+                <p> {ad?.blood}</p>
+                <p> {ad?.gender}</p>
+                <p className="text-xs md:text-sm"> {ad?.email}</p>
               </div>
               <div className="text-start md:text-center  lg:text-center xl:text-start ">
                 <button className="btn btn-secondary  btn-sm  ">
